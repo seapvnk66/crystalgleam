45376a45f1c1aec9ec5c527674724c7232bd0a1c
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-05-30 18:18:04 -0400

    Fix fountain tile animation without slowing down turbine

--- data/tileset_anims.asm
+++ data/tileset_anims.asm
@@ -247,13 +247,13 @@ TilesetSnowtopMountainAnim::
 
 TilesetFacilityAnim::
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe AnimateFountainTile,      $0:4d
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe DoNothing
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe DoNothing
 	tileframe AnimateTurbineTiles,      $0:6c
 	tileframe StandingTileFrame8
@@ -261,13 +261,13 @@ TilesetFacilityAnim::
 
 TilesetHideoutAnim::
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe AnimateSpinnerTile,       $0:07
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe DoNothing
 	tileframe AnimateTurbineTiles,      $0:6c
-	tileframe StandingTileFrame8
+	tileframe DoNothing
 	tileframe DoNothing
 	tileframe AnimateTurbineTiles,      $0:6c
 	tileframe StandingTileFrame8
